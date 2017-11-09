11c9aa1c2379cc1eaeb0e59baaad24c37008d0c9
Kalier Howard <email": null>
2017-11-09 17:23:51 -0600

    add navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,40 @@
 class User < ApplicationRecord
+   include BCrypt
+
   has_many :questions, foreign_key: :asker_id
   has_many :answers, foreign_key: :creator_id
   has_many :votes, foreign_key: :voter_id
   has_many :comments, foreign_key: :commenter_id
 
+  validates :username, :email, presence: true
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
+
+  validate :validate_password
+
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_plain_text_password)
+    @raw_password = new_plain_text_password
+    @password = Password.create(new_plain_text_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate(plain_text_password)
+    self.password == plain_text_password
+  end
+
+  def validate_password
+    if @raw_password.length == 0
+      errors.add(:password, "is required")
+    elsif
+      @raw_password.length < 8
+      errors.add(:password, "must be at least 8 characters")
+    end
+  end
 
 
 end
